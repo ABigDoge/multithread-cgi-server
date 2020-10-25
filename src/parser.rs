f9c4bc15f221b0d28d0b29b66ef5441e13693519
delirio <delirio579@163.com>
2020-10-25 13:45:13 +0800

    error returning Request

--- src/parser.rs
+++ src/parser.rs
@@ -3,18 +3,25 @@ extern crate httparse;
 use httparse::Request;
 
 #[derive(Debug)]
-pub struct Pkg {
+pub struct Loginfo {
     method : String,
     host : String,
     user : String,
+    path : String,
     length : usize,
     iscgi : bool
 }
 
-pub fn parser (s : String) -> Pkg {
+#[derive(Debug)]
+pub struct Pkg<'headers, 'buf> {
+    pub log : Loginfo,
+    pub req : Request<'headers, 'buf>
+}
+
+pub fn parser (s : String) -> Pkg<'static, 'static> {
 
     let le = s.len();
-    let su = s.as_bytes();
+    let su = &s.as_bytes();
     let mut headers = [httparse::EMPTY_HEADER; 16];
     let mut req = Request::new(&mut headers[..]);
     let res = req.parse(su).unwrap();
@@ -24,13 +31,36 @@ pub fn parser (s : String) -> Pkg {
     if partpath == "/cgi-bin" {
         iscgi = true;
     }
+
+    let mut host = String::new();
+    let mut user = String::new();
+
+    let mut index = 0;
+    while index < 10 {
+        let i = req.headers[index];
+        if i.name.to_string() == "Host" {
+            host = String::from_utf8(i.value.to_vec()).unwrap();
+        }
+
+        if i.name.to_string() == "User-Agent" {
+            user = String::from_utf8(i.value.to_vec()).unwrap();
+        }
+
+        index = index + 1;
+    }
+
+
+    let log = Loginfo {
+        method: req.method.unwrap().to_string(),
+        host: host,
+        user: user,
+        path: req.path.unwrap().to_string(),
+        length: le,
+        iscgi: iscgi,
+    };
     
     Pkg {
-            method: req.method.unwrap().to_string(),
-            host: String::from_utf8(req.headers[0].value.to_vec()).unwrap(),
-            user: String::from_utf8(req.headers[4].value.to_vec()).unwrap(),
-            length: le,
-            iscgi: iscgi
+            log : log,
+            req : req
         }
 }
-
